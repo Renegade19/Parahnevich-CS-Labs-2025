5e407036fbc9d2106bf87841504439a84546103e
Алексей Парахневич <porox2006@mail.ru>
2025-11-07 00:54:46 +0300

    comment

--- rk.py
+++ rk.py
@@ -1,98 +1,94 @@
 from operator import itemgetter
-class Dir:
-    "Дирижёр"
-    def __init__(self, id, fio, experience, orch_id):
-        self.id = id
-        self.fio = fio
-        self.experience = experience
-        self.orch_id = orch_id
 
-class Orch:
+class Conductor:
+    "Дирижёр"
+    def __init__(self, id, name, experience, orchestra_id):
+        self.id = id
+        self.name = name
+        self.experience = experience
+        self.orchestra_id = orchestra_id
+
+class Orchestra:
     "Оркестр"
     def __init__(self, id, name):
         self.id = id
         self.name = name
 
-class DirOrch:
-    "Дирижёры оркестра (связь многие-ко-многим)"
-    def __init__(self, dir_id, orch_id):
-        self.dir_id = dir_id
-        self.orch_id = orch_id
+class ConductorOrchestra:
+    "Связь 'Дирижёры оркестров' для реализации многие-ко-многим"
+    def __init__(self, orchestra_id, conductor_id):
+        self.orchestra_id = orchestra_id
+        self.conductor_id = conductor_id
 
 
 orchestras = [
-    Orch(1, "Симфонический оркестр Москвы"),
-    Orch(2, "Филармонический оркестр Санкт-Петербурга"),
-    Orch(3, "Камерный оркестр России"),
-    Orch(4, "Большой оркестр"),
-    Orch(5, "Джазовый оркестр города")
+    Orchestra(1, 'Арбатский оркестр'),
+    Orchestra(2, 'Ленинградский оркестр'),
+    Orchestra(3, 'Тверской оркестр'),
+    Orchestra(4, 'Академический оркестр'),
+    Orchestra(5, 'Профсоюзный оркестр'),
 ]
 
 conductors = [
-    Dir(1, "Артамонов", 15, 1),
-    Dir(2, "Петров", 20, 2),
-    Dir(3, "Иваненко", 10, 3),
-    Dir(4, "Иванов", 12, 3),
-    Dir(5, "Иванин", 8, 3),
+    Conductor(1, 'Артамонов', 5, 1),
+    Conductor(2, 'Петров', 8, 2),
+    Conductor(3, 'Иваненко', 12, 3),
+    Conductor(4, 'Иванов', 9, 3),
+    Conductor(5, 'Сидоров', 4, 4),
+    Conductor(6, 'Алексеев', 15, 4),
 ]
 
-dirs_orchs = [
-    DirOrch(1, 1),
-    DirOrch(2, 2),
-    DirOrch(3, 3),
-    DirOrch(4, 3),
-    DirOrch(5, 3),
-    DirOrch(1, 4),
-    DirOrch(2, 5),
-    DirOrch(3, 5),
+# Связи многие-ко-многим
+conductors_orchestras = [
+    ConductorOrchestra(1, 1),
+    ConductorOrchestra(2, 2),
+    ConductorOrchestra(3, 3),
+    ConductorOrchestra(3, 4),
+    ConductorOrchestra(4, 5),
+    ConductorOrchestra(4, 6),
+    ConductorOrchestra(5, 2),
 ]
 
 def main():
     "Основная функция"
-    one_to_many = [(d.fio, d.experience, o.name)
+    one_to_many = [(c.name, c.experience, o.name)
                    for o in orchestras
-                   for d in conductors
-                   if d.orch_id == o.id]
+                   for c in conductors
+                   if c.orchestra_id == o.id]
 
-    many_to_many_temp = [(o.name, do.orch_id, do.dir_id)
+    many_to_many_temp = [(o.name, co.orchestra_id, co.conductor_id)
                          for o in orchestras
-                         for do in dirs_orchs
-                         if o.id == do.orch_id]
+                         for co in conductors_orchestras
+                         if o.id == co.orchestra_id]
 
-    many_to_many = [(d.fio, d.experience, orch_name)
-                    for orch_name, orch_id, dir_id in many_to_many_temp
-                    for d in conductors if d.id == dir_id]
+    many_to_many = [(c.name, c.experience, orchestra_name)
+                    for orchestra_name, orchestra_id, conductor_id in many_to_many_temp
+                    for c in conductors if c.id == conductor_id]
 
+    print('Задание Г1')
+    res_1 = []
+    for o in orchestras:
+        if o.name.startswith('А'):
+            o_conductors = list(filter(lambda i: i[2] == o.name, one_to_many))
+            if len(o_conductors) > 0:
+                res_1.append((o.name, [name for name, _, _ in o_conductors]))
+    print(res_1)
 
-    print("Задание А1")
-    res_1 = sorted(one_to_many, key=itemgetter(2))
-    for r in res_1:
-        print(r)
-
-    print("\nЗадание А2")
+    print('\nЗадание Г2')
     res_2_unsorted = []
     for o in orchestras:
-        d_list = list(filter(lambda x: x[2] == o.name, one_to_many))
-        if len(d_list) > 0:
-            exp_list = [exp for _, exp, _ in d_list]
-            exp_sum = sum(exp_list)
-            res_2_unsorted.append((o.name, exp_sum))
-
+        o_conductors = list(filter(lambda i: i[2] == o.name, one_to_many))
+        if len(o_conductors) > 0:
+            experiences = [exp for _, exp, _ in o_conductors]
+            max_exp = max(experiences)
+            res_2_unsorted.append((o.name, max_exp))
     res_2 = sorted(res_2_unsorted, key=itemgetter(1), reverse=True)
-    for r in res_2:
-        print(r)
+    print(res_2)
 
-    print("\nЗадание А3")
-    res_3 = {}
-    for o in orchestras:
-        if "оркестр" in o.name.lower():
-            o_dirs = list(filter(lambda x: x[2] == o.name, many_to_many))
-            o_dir_names = [x for x, _, _ in o_dirs]
-            res_3[o.name] = o_dir_names
-
-    for k, v in res_3.items():
-        print(f"{k}: {', '.join(v)}")
+    print('\nЗадание Г3')
+    res_3 = sorted(many_to_many, key=itemgetter(2))
+    print(res_3)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
